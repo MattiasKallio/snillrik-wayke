28cf3c583ce3ffd97486b106afb8729a7fdca276
Mattias Kallio <kallio@snillrik.se>
2022-10-20 21:16:11 +0200

    Update SNWK_Shortcodes.php

--- classes/SNWK_Shortcodes.php
+++ classes/SNWK_Shortcodes.php
@@ -15,16 +15,17 @@ class SNWK_shortcodes
         add_shortcode('wayke_price', array($this, 'wayke_price'));
     }
 
-    public function wayke_price(){
+    public function wayke_price()
+    {
         global $snillrik_price;
-        $ret_str = "<div class='snillrik-waykeunder-info'><span class='snillrik-waykeprice'>" .SNWK_DesignElements::format_price($snillrik_price) . " kr</span>Pris</div>";
+        $ret_str = "<div class='snillrik-waykeunder-info'><span class='snillrik-waykeprice'>" . SNWK_DesignElements::format_price($snillrik_price) . " kr</span>Pris</div>";
         return $ret_str;
     }
 
     public function wayke_list($atts)
-    { 
+    {
         global $wp;
-        $thispage = home_url( $wp->request );
+        $thispage = home_url($wp->request);
         $attributes = shortcode_atts(array(
             'hits' => 12,
         ), $atts);
@@ -35,54 +36,53 @@ class SNWK_shortcodes
         $api = new SNWK_API();
         $args = $_GET;
 
-        
+
         $args["hits"] = $attributes['hits'];
 
-        $call = $api->wayke_call("vehicles",$args);
-        
+        $call = $api->wayke_call("vehicles", $args);
+        //print_r($call);
+        if (isset($call->message)) {
+            return $call->message;
+        }
         $str_out .= SNWK_DesignElements::search_box($thispage, $call->facets);
 
-        if($call && isset($call->documentList->numberOfHits) && $call->documentList->numberOfHits>0){
-            foreach($call->documentList->documents as $item){
+        if ($call && isset($call->documentList->numberOfHits) && $call->documentList->numberOfHits > 0) {
+            foreach ($call->documentList->documents as $item) {
                 $str_out .= SNWK_DesignElements::standard_box($item);
             }
         }
 
         $pageination = $call->documentList->pagination->pages;
 
-        foreach($pageination as $page){
-            $page_str = "<a href='$thispage/?".$page->query."'>".$page->displayName."</a>";
+        foreach ($pageination as $page) {
+            $page_str = "<a href='$thispage/?" . $page->query . "'>" . $page->displayName . "</a>";
             $pageination_str .= "<div class='pagination-number'>$page_str</div>";
         }
 
-        return "<div class='snillrik-waykelist-main'>".$str_out."<div class='snillrik-waykepaginator'>$pageination_str</div></div>";
+        return "<div class='snillrik-waykelist-main'>" . $str_out . "<div class='snillrik-waykepaginator'>$pageination_str</div></div>";
     }
     public function wayke_item($atts)
-    { 
+    {
         $attributes = shortcode_atts(array(
             'rightcol' => true,
         ), $atts);
-        
+
         $the_car_id = get_query_var("bilid", 0);
         $str_out = "";
         global $snillrik_price;
         $snillrik_price = 0;
         $api = new SNWK_API();
         //$call = $api->wayke_call("vehicle", ["id"=>"b88614ff-c004-41e8-8ff8-21fa0283318f"]);
-        $call = $api->wayke_call("vehicle", ["id"=>$the_car_id]);
-        
+        $call = $api->wayke_call("vehicle", ["id" => $the_car_id]);
+
         $snillrik_price = isset($call->documentList->documents[0]) ? $call->documentList->documents[0]->price : 0;
 
-       //$str_out = print_r($call,true);
-       if($call && isset($call->documentList->numberOfHits) && $call->documentList->numberOfHits>0){
+        //$str_out = print_r($call,true);
+        if ($call && isset($call->documentList->numberOfHits) && $call->documentList->numberOfHits > 0) {
             $item = $call->documentList->documents[0];
             $str_out =  SNWK_DesignElements::main_box($item, $attributes['rightcol']);
         }
 
         return $str_out;
-    }   
-    
-    
-
+    }
 }
-
